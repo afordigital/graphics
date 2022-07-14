6cd440f5ec5fe5ee4465d302ac1ffcd612b101fc
Aforina <43246362+Aforina@users.noreply.github.com>
2022-07-14 22:08:42 +0200

    details about design

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import LineChart2 from './components/LineChart2'
 function App () {
   return (
     <div className='bg-[#f5f5f5] font-Montserrat h-full'>
-      <div className='max-w-4xl mx-auto py-24 space-y-8'>
+      <div className='max-w-4xl mx-auto pt-24 pb-60 space-y-8'>
         <div className='bg-white p-2 lg:p-12 rounded-2xl drop-shadow-lg'>
           <LineChart />
         </div>
