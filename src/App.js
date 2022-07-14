8b400950343a8134467ed0d3de728637b04de9a6
Aforina <43246362+Aforina@users.noreply.github.com>
2022-07-14 18:47:46 +0200

    bg problem solved

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import LineChart2 from './components/LineChart2'
 
 function App () {
   return (
-    <div className='bg-[#f5f5f5] font-Montserrat'>
+    <div className='bg-[#f5f5f5] font-Montserrat h-full'>
       <div className='max-w-4xl mx-auto py-24 space-y-8'>
         <div className='bg-white p-2 lg:p-12 rounded-2xl drop-shadow-lg'>
           <LineChart />
